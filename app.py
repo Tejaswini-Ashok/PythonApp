f0353c9e7f0161d8c063e89bce1c53c88a1c2e7a
Tejaswini-Ashok <168276324+Tejaswini-Ashok@users.noreply.github.com>
2024-05-12 15:45:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
-def sanath():
-    return "Hello World from Sanath Kumar along with his puppy!"
+def new1():
+    return "Hello !"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
